5c397c1732d72cfe2f5df5ebb56b34c5604a326c
Myroslav Rys <stonevil@gmail.com>
2015-03-04 10:11:42 +0200

    Rewrite print procedure. Merge vm_icon and vm_status procedures

--- scripts/vm_status.sh
+++ scripts/vm_status.sh
@@ -4,7 +4,21 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source "$CURRENT_DIR/helpers.sh"
 
-print_vm_status() {
+vm_option_string="@vm_icon"
+
+vm_icon_osx="⧉ "
+vm_icon="VM "
+
+vm_icon_default() {
+	if is_osx; then
+		echo "$vm_icon_osx"
+	else
+		echo "$vm_icon"
+	fi
+}
+
+vm_count() {
+  VM="0"
 	if is_osx; then
     if [[ -d '/Applications/VMware Fusion.app' ]]; then
       VM=$(($VM+`"/Applications/VMware Fusion.app/Contents/Library/vmrun" -T fusion list | awk 'NR==1{print $4}'`))
@@ -20,7 +34,16 @@ print_vm_status() {
 	if command_exists "VBoxManage"; then
     VM=$(($VM+`VBoxManage list runningvms | wc -l`))
 	fi
+
   echo "$VM"
+}
+
+print_vm_status() {
+	# spacer fixes weird emoji spacing
+	local spacer=" "
+  if [ $(vm_count) -ne 0 ]; then
+    printf "$(get_tmux_option "$vm_option_string" "$(vm_icon_default)")$spacer$(vm_count)"
+	fi
 }
 
 main() {
